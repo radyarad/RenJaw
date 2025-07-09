62ea500f0ce6fabc274427d0495f5f2c8ce41cea
muhammad syahradya <muh.syahradya@gmail.com>
2025-07-09 10:15:08 +0700

    fixpull

--- src/app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Login() {
-  return (
-    <main className="flex flex-col ">
-      <div className="absolute top-[5%]">
-        <h1>Login Page</h1>
-      </div>
-    </main>
-  );
-}
